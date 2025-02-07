ceb9373abf09a915fa4a65f6a7537b5643379f22
Vagarth <vagarth419@gmail.com>
2025-02-07 11:58:31 +0530

    Initial Commit

---
+++ Fit24-7/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 07/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fit24-7/Fit24_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Fit24_7App.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Fit24_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
