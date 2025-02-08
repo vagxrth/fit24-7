143dde5c7fa321c056ffc5c6d76cf2f35d708eb0
Vagarth <vagarth419@gmail.com>
2025-02-08 13:22:11 +0530

    feat: add calories, active and standing session in home view

--- Fit24-7/HomeView.swift
+++ Fit24-7/HomeView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                Text("Welcome to Fit24-7")
+                    .font(.largeTitle)
+                    .padding()
+                HStack {
+                    VStack {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Calories")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.red)
+                            Text("123 Kcal")
+                                .bold()
+                        }
+                        .padding(.bottom)
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Active")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.green)
+                            Text("52 mins")
+                                .bold()
+                        }
+                        .padding(.bottom)
+
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text("Standing")
+                                .font(.callout)
+                                .bold()
+                                .foregroundColor(.blue)
+                            Text("4 hours")
+                                .bold()
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
