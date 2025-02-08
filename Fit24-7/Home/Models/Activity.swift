267279655a0e64c238b3eb482c7012cec713f485
Vagarth <vagarth419@gmail.com>
2025-02-08 18:39:30 +0530

    chore: modify the folder structure for better grouping

---
+++ Fit24-7/Home/Models/Activity.swift
@@ -0,0 +1,17 @@
+//
+//  Activity.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 08/02/25.
+//
+
+import SwiftUI
+
+struct Activity {
+    let id: Int
+    let title: String
+    let subtitle: String
+    let image: String
+    let tintColor: Color
+    let data: String
+}
---
+++ Fit24-7/Home/Models/Workout.swift
@@ -0,0 +1,18 @@
+//
+//  Workout.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 08/02/25.
+//
+
+import SwiftUI
+
+struct Workout {
+    let id: Int
+    let title: String
+    let image: String
+    let tintColor: Color
+    let duration: String
+    let date: String
+    let calories: String
+}
