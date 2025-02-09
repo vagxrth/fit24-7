3b751a3c6af5dfd6f4b48cfece1df4c09e90868f
Vagarth <vagarth419@gmail.com>
2025-02-09 12:56:29 +0530

    feat: create charts for the activity

---
+++ Fit24-7/ChartsView.swift
@@ -0,0 +1,56 @@
+//
+//  HistoricDataView.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 08/02/25.
+//
+
+import SwiftUI
+import Charts
+
+struct DailyStepModel: Identifiable {
+    let id = UUID()
+    let date: Date
+    let count: Double
+}
+
+class ChartsViewModel: ObservableObject {
+    var mockChartData = [
+        DailyStepModel(date: Date(), count: 6400),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date(), count: 9740),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -2, to: Date()) ?? Date(), count: 6500),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -3, to: Date()) ?? Date(), count: 10300),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -4, to: Date()) ?? Date(), count: 7225),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -5, to: Date()) ?? Date(), count: 8278),
+        DailyStepModel(date: Calendar.current.date(byAdding: .day, value: -6, to: Date()) ?? Date(), count: 3453)
+    ]
+}
+
+struct ChartsView: View {
+    
+    @StateObject var viewModel = ChartsViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("Charts")
+                .font(.largeTitle)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            
+            Chart {
+                ForEach(viewModel.mockChartData) {
+                    data in BarMark(x: .value(data.date.formatted(), data.date, unit: .day), y: .value("Steps", data.count))
+                }
+            }
+            .frame(maxHeight: 350)
+            .padding(.horizontal)
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+    }
+}
+
+#Preview {
+    ChartsView()
+}
