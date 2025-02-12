21be342b4b2507f33c6c42fd5269e8e3c02686d9
Vagarth <vagarth419@gmail.com>
2025-02-12 08:28:36 +0530

    feat: create function to get user step counts

--- Fit24-7/LeaderboardView.swift
+++ Fit24-7/LeaderboardView.swift
@@ -8,24 +8,23 @@
 import SwiftUI
 
 struct LeaderboardUser: Codable, Identifiable {
-    let id: Int
-    let createdAt: String
+    var id = UUID()
     let username: String
     let count: Int
 }
 
 class LeaderboardViewModel: ObservableObject {
     var mockData = [
-        LeaderboardUser(id: 0, createdAt: "", username: "john", count: 2122),
-        LeaderboardUser(id: 1, createdAt: "", username: "seth", count: 4332),
-        LeaderboardUser(id: 2, createdAt: "", username: "drew", count: 3711),
-        LeaderboardUser(id: 3, createdAt: "", username: "mark", count: 6134),
-        LeaderboardUser(id: 4, createdAt: "", username: "pat", count: 1123),
-        LeaderboardUser(id: 5, createdAt: "", username: "paul", count: 2122),
-        LeaderboardUser(id: 6, createdAt: "", username: "sam", count: 4332),
-        LeaderboardUser(id: 7, createdAt: "", username: "elon", count: 3711),
-        LeaderboardUser(id: 8, createdAt: "", username: "garry", count: 6134),
-        LeaderboardUser(id: 9, createdAt: "", username: "peter", count: 1123)
+        LeaderboardUser(username: "john", count: 2122),
+        LeaderboardUser(username: "seth", count: 4332),
+        LeaderboardUser(username: "drew", count: 3711),
+        LeaderboardUser(username: "mark", count: 6134),
+        LeaderboardUser(username: "pat", count: 1123),
+        LeaderboardUser(username: "paul", count: 2122),
+        LeaderboardUser(username: "sam", count: 4332),
+        LeaderboardUser(username: "elon", count: 3711),
+        LeaderboardUser(username: "garry", count: 6134),
+        LeaderboardUser(username: "peter", count: 1123)
     ]
 }
 
--- Fit24-7/Managers/DatabaseManager.swift
+++ Fit24-7/Managers/DatabaseManager.swift
@@ -16,11 +16,17 @@ class DatabaseManager {
     }
     
     let database = Firestore.firestore()
+    private let weeklyLeaderboard = "\(Date().mondayDateFormat())-leaderboard"
     
-    func fetchLeaderboard() async throws {
-        let snapshot = try await database.collection("-leaderboard").getDocuments()
-        
-        print(snapshot.documents)
-        print(snapshot.documents.first?.data())
+    func fetchLeaderboard() async throws -> [LeaderboardUser] {
+        let snapshot = try await database.collection(weeklyLeaderboard).getDocuments()
+        return try snapshot.documents.compactMap({
+            try $0.data(as: LeaderboardUser.self)
+        })
+    }
+    
+    func postStepCountUpdateForUser(leader: LeaderboardUser) async throws {
+        let data = try Firestore.Encoder().encode(leader)
+        try await database.collection(weeklyLeaderboard).document(leader.username).setData(data, merge: false)
     }
 }
