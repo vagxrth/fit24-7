e8bb3b8bdc4e1bcf083ec1b8474e797d8e6c5f26
Vagarth <vagarth419@gmail.com>
2025-02-13 22:30:11 +0530

    ui: add avatar icons

---
+++ Fit24-7/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 13/02/25.
+//
+
+import SwiftUI
+
+struct ProfileView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ProfileView()
+}
