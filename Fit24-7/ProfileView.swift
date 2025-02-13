e02433ea37a405c7cf3c34bc181d514211732c8a
Vagarth <vagarth419@gmail.com>
2025-02-13 22:42:03 +0530

    feat: create top section of profile view with avatar, greeting and name

--- Fit24-7/ProfileView.swift
+++ Fit24-7/ProfileView.swift
@@ -10,8 +10,27 @@ import SwiftUI
 struct ProfileView: View {
     var body: some View {
         VStack {
-            
+            HStack(spacing: 16) {
+                Image("machine")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .padding(.all, 8)
+                    .background(
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(.gray.opacity(0.4))
+                    )
+                VStack(alignment: .leading) {
+                    Text("Good Morning, ")
+                        .font(.largeTitle)
+                        .foregroundColor(.gray)
+                    Text("Vagarth")
+                        .font(.title)
+                }
+            }
         }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
     }
 }
 
