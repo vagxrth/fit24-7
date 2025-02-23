c1391be27bd3fe24f1a4152acb9eed25b9075d10
Vagarth <vagarth419@gmail.com>
2025-02-23 13:29:55 +0530

    feat: create a model for year chart data

---
+++ Fit24-7/Charts/Models/YearChartData.swift
@@ -0,0 +1,13 @@
+//
+//  YearChartData.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 23/02/25.
+//
+
+import Foundation
+
+struct YearChartDataResult {
+    let ytd: [MonthlyStepModel]
+    let oneYear: [MonthlyStepModel]
+}
