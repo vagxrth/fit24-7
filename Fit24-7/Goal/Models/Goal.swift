41ce3db14eb2b6b57e5e4825da8e0f2d13335c91
Vagarth <vagarth419@gmail.com>
2025-02-23 13:53:00 +0530

    feat: create a goal model

---
+++ Fit24-7/Goal/Models/Goal.swift
@@ -0,0 +1,15 @@
+//
+//  Goal.swift
+//  Fit24-7
+//
+//  Created by Vagarth Pandey on 23/02/25.
+//
+
+import Foundation
+
+struct Goal {
+    var goalType: String
+    var targetWeight: Double?
+    var workoutPreference: String?
+    var dailyCalorieIntake: Int?
+}
